08f76438786cc2f2c3ad5c0676c1da14a0df360d
warrend <warrend555@gmail.com>
2017-01-02 01:38:17 -0500

    refactor news

--- lib/threecast/news.rb
+++ lib/threecast/news.rb
@@ -17,31 +17,20 @@ class Threecast::News
 
   def self.scrape_cnn
     doc = Nokogiri::HTML(open("https://twitter.com/cnn"))
-
-    news = self.new
-    news.story = doc.search(".js-tweet-text-container p").first.text
-    news.url = doc.search(".js-tweet-text-container p a").attr("href").value
-    news.network = doc.search(".account-group .fullname").first.text
-    news.date = doc.search(".stream-item-header .time a").attr("title").value
-
-    news
+    self.news_creator(doc)
   end
 
   def self.scrape_msnbc
     doc = Nokogiri::HTML(open("https://twitter.com/msnbc"))
-
-    news = self.new
-    news.story = doc.search(".js-tweet-text-container p").first.text
-    news.url = doc.search(".js-tweet-text-container p a").attr("href").value
-    news.network = doc.search(".account-group .fullname").first.text
-    news.date = doc.search(".stream-item-header .time a").attr("title").value
-
-    news
+    self.news_creator(doc)
   end
 
   def self.scrape_foxnews
     doc = Nokogiri::HTML(open("https://twitter.com/foxnews"))
+    self.news_creator(doc)
+  end
 
+  def self.news_creator(doc)
     news = self.new
     news.story = doc.search(".js-tweet-text-container p").first.text
     news.url = doc.search(".js-tweet-text-container p a").attr("href").value
